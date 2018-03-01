fe786105270a4d322294387fc9df580c0466dc55
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-03-01 09:12:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,24 @@
-var api = "http://apihost.com:port";
+var api = "http://crep.npool.pw:8118";
 
-var poolHost = "apihost.com";
+var poolHost = "crep.npool.pw";
 
-var blockExplorer = "http://blockexplorer.pw/coin";
+var blockExplorer = "http://blocks.crepcoin.org/";
 
 var cryptonatorWidget = ["BTC-USD", "XMR-USD"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+var blockchainExplorer = "http://blocks.crepcoin.org/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+var transactionExplorer = "http://blocks.crepcoin.org/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["coin.apihost.com", "http://coin.apihost.com:port"],
+        ["pool.crepcoin.org", "http://pool.crepcoin.org:8117"],
+        ["cryptoknight.cc/crep", "http://78.46.85.142:6338"],
+        ["labbinarymining.com", "http://crep-us.labbinarymining.com:8117"],
+        ["crep.hashblocks.top", "http://hashblocks.top:39828"],
+        ["poolsofaus.com", "http://poolsofaus.com:8117"],
+        ["npool.pw/crepcoin", "http://crep.npool.pw:8118"]
 ];
